39a78e5c604fb0ba1eba3c49f0fc3e19a7613f6d
justmic007 <justmic007@gmail.com>
2019-01-01 13:29:59 +0100

    Grab UI variables

---
+++ app.js
@@ -0,0 +1,20 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate Results
+function calculateResults(e) {
+  console.log('Calculate Results');
+  // Grab all the UI variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalInterest = document.getElementById('total-interest');
+  const totalPayment = document.getElementById('total-payment');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12
+
+  e.preventDefault(); // prevents form default behaviour
+}
